fa9001a74e0f7888dc8240316760b152b934a48a
gardan4 <marcmeijers01@gmail.com>
2023-12-02 19:08:49 +0100

    init of main

--- model_builder.py
+++ model_builder.py
@@ -1,44 +1,44 @@
 # create model class with x and y FOR INPUT
-import numpy as np
-import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
-from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
 
 from selfmade_lime import lime_explainer
 from lime.lime_tabular import LimeTabularExplainer
 
 
+
 class ModelBuild:
     def __init__(self, model_type):
         if model_type == 'DecisionTree':
-            self.dtree = DecisionTreeClassifier()
+            self.model = DecisionTreeClassifier()
+        elif model_type == 'Logistic':
+            print('logistic regression selected')
+            self.model = LogisticRegression()
         else:
             print('default Decision Tree Classifier selected')
-            self.dtree = DecisionTreeClassifier()
+            self.model = DecisionTreeClassifier()
 
     def train_eval(self, X_train, y_train, X_test, y_test):
-        self.dtree.fit(X_train, y_train)
-        y_pred = self.dtree.predict(X_test)
+        self.model.fit(X_train, y_train)
+        y_pred = self.model.predict(X_test)
         accuracy = accuracy_score(y_test, y_pred)
-        class_report = classification_report(y_test, y_pred)
         cm = confusion_matrix(y_test, y_pred)
-        return self.dtree
+        return self.model, accuracy, cm
 
     @staticmethod
-    def selfmade_explain(X_train):
-        self_lime = lime_explainer.LimeTabularExplainer(training_data=np.array(X_train),
-                                                        feature_names=X_train.columns,
+    def selfmade_explain(X_train, X_cols):
+        self_lime = lime_explainer.LimeTabularExplainer(training_data=X_train,
+                                                        feature_names=X_cols,
                                                         class_names=['bad', 'good'],
                                                         mode='classification')
         return self_lime
 
     @staticmethod
-    def explain(X_train):
-        real_lime = LimeTabularExplainer(training_data=np.array(X_train),
-                                         feature_names=X_train.columns,
+    def explain(X_train, X_cols):
+        real_lime = LimeTabularExplainer(training_data=X_train,
+                                         feature_names=X_cols,
                                          class_names=['bad', 'good'],
                                          mode='classification')
         return real_lime
